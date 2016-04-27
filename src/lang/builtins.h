089dedee844ee4579dd99eb22ff56333516de227
Andy Aylward <aaylward@gmail.com>
2016-04-27 00:33:57 -0400

    removing unused includes (#8)

--- src/lang/builtins.h
+++ src/lang/builtins.h
@@ -1,8 +1,6 @@
 #ifndef TOYPPELBUILTINS
 #define TOYPPELBUILTINS
 
-#include <iostream>
-#include <utility>
 #include <unordered_map>
 #include <string>
 
@@ -11,10 +9,6 @@
 
 using std::unordered_map;
 using std::string;
-using std::pair;
-using std::make_pair;
-using std::cout;
-using std::endl;
 
 namespace toyppellanger {
   namespace lang {
