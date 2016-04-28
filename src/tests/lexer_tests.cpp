7afe4724a2688cac2f91688888f03380ccf4d744
Andy Aylward <aaylward@gmail.com>
2016-04-28 14:42:37 -0400

    tweak lexer + add test (#14)

--- src/tests/lexer_tests.cpp
+++ src/tests/lexer_tests.cpp
@@ -12,10 +12,10 @@ using std::pair;
 using std::cout;
 using std::endl;
 
-using toyppellanger::ToyppelLangerLexer;
+using toyppellanger::ToyppelLexer;
 
 vector<string> getTokens(const string& program) {
-  ToyppelLangerLexer l(program);
+  ToyppelLexer l(program);
   vector<string> tokens;
 
   pair<bool, string> next;
@@ -27,15 +27,37 @@ vector<string> getTokens(const string& program) {
   return tokens;
 }
 
-void itCanReadStrings() {
+void report(const char* test, bool& passing, vector<string>& actual, vector<string>& expected) {
+  if (actual == expected) {
+    cout << "[SUCCESS] : " << test << " ✓" <<endl;
+  } else {
+    cout << "[FAILURE] : " << test << " ✗" <<endl;
+    passing = false;
+  }
+}
+
+void itCanReadStringsInsideParens(bool& passing) {
   auto program = "yo(\"ham\");";
   vector<string> tokens = getTokens(program);
   vector<string> expected = {"yo", "(", "\"ham\"", ")", ";"};
-  assert(tokens == expected);
+  report("itCanReadStringsInsideParens", passing, tokens, expected);
+}
+
+void itCanReadParensWithAndWithoutSpaces(bool& passing) {
+  auto program = "((( )())   )";
+  vector<string> tokens = getTokens(program);
+  vector<string> expected = {"(", "(", "(", ")", "(", ")", ")", ")"};
+  report("itCanReadParensWithAndWithoutSpaces", passing, tokens, expected);
 }
 
 int main() {
-  itCanReadStrings();
-  cout << "[LEXER TESTS] : ALL PASSED" << endl;
-  return 0;
+  bool passing = true;
+  itCanReadStringsInsideParens(passing);
+  itCanReadParensWithAndWithoutSpaces(passing);
+  if (passing) {
+    cout << "[LEXER TESTS] : ALL PASSED" << endl;
+    return 0;
+  }
+  cout << "[LEXER TESTS] : FAILED" << endl;
+  return -1;
 }
